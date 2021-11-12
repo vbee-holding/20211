a00ac2fb77774fe5f3146da878f4a5a282b01711
20194266-halt <91833527+20194266-halt@users.noreply.github.com>
2021-11-12 20:50:11 +0700

    Create controller.js

---
+++ week-7/controller/controller.js
@@ -0,0 +1,22 @@
+let User = require('../model/model');
+const updateUser = (req, res)=>{
+    if(!req.body){
+        return res
+            .status(400)
+            .send({ message : "Data to update can not be empty"})
+    }
+
+    const id = req.params.id;
+    User.findByIdAndUpdate(id, req.body, { useFindAndModify: false})
+        .then(data => {
+            if(!data){
+                res.status(404).send({ message : `Cannot Update user with ${id}. Maybe user not found!`})
+            }else{
+                res.send(data)
+            }
+        })
+        .catch(err =>{
+            res.status(500).send({ message : "Error Update user information"})
+        })
+}
+module.exports = { updateUser}
