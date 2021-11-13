1637a471fe3b624f994fa837656062f86d9d9371
20194266-halt <91833527+20194266-halt@users.noreply.github.com>
2021-11-13 09:24:35 +0700

    Create router.js

---
+++ week-7/20194266/routes/router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+const controller = require('../controller/controller');
+
+
+route.get('/', services.homeRoutes);
+
+
+route.get('/addUser', services.addUser)
+
+route.get('/updateUser', services.updateUser)
+route.get('/',controller.getAllUser);
+
+
+module.exports = route
