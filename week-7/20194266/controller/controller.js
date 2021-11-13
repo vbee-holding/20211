db9f021ef5f86037f37e0b317ce0294f207b5997
20194266-halt <91833527+20194266-halt@users.noreply.github.com>
2021-11-13 07:18:34 +0700

    Create controller.js

---
+++ week-7/20194266/controller/controller.js
@@ -0,0 +1,31 @@
+let User = require('../model/model');
+const findUser = (req, res)=>{
+
+    if(req.query.id){
+        const id = req.query.id;
+
+        User.findById(id)
+            .then(data =>{
+                if(!data){
+                    res.status(404).send({ message : "Not found user with id "+ id})
+                }else{
+                    res.send(data)
+                }
+            })
+            .catch(err =>{
+                res.status(500).send({ message: "Error retrieving user with id " + id})
+            })
+
+    }else{
+        User.find()
+            .then(user => {
+                res.send(user)
+            })
+            .catch(err => {
+                res.status(500).send({ message : err.message || "Error Occurred while retriving user information" })
+            })
+    }
+
+    
+}
+module.exports = {  findUser}
